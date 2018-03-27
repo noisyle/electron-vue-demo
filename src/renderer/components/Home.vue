f859570aa8238a03e0a253d75ce7713e3f358f06
York <anyone.1984@gmail.com>
2018-03-28 00:10:18 +0800

    first commit

---
+++ src/renderer/components/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="wrapper">
+    <div class="settings">
+      <div class="container">
+        <div class="field">
+          <label class="label">输入网址</label>
+          <div class="control">
+            <textarea class="textarea" placeholder="http(s)://..." v-model="url"></textarea>
+          </div>
+        </div>
+        <div class="field is-grouped">
+          <div class="control">
+            <button class="button is-link" @click="start()">开始</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <webview class="webview" src="https://www.baidu.com/" partition="persist:webviewsession"></webview>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+    data () {
+      return {
+        url: 'https://www.baidu.com/'
+      }
+    },
+    methods: {
+      start () {
+        const webview = document.querySelector('.webview')
+        webview.loadURL(this.url)
+      }
+    }
+  }
+</script>
+
+<style>
+html {
+  overflow: hidden;
+}
+.wrapper {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  flex-flow: row nowrap;
+}
+.wrapper .settings {
+  width: 300px;
+  border-right: 1px solid #bbb;
+}
+.wrapper .webview {
+  flex: auto;
+}
+</style>
